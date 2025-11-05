26a046dba8a6681e9a6e211792f7edeecb19ec6f
Ushant Singh <ushantsingh4@gmail.com>
2025-11-05 23:46:21 +0530

    Q9

---
+++ 15Questions.java/Question9.java
@@ -0,0 +1,9 @@
+// Type casting example: Convert float to int and print both
+
+public class Question9 {
+    public static void main(String[] args) {
+        int a = 5;
+        byte b = (byte) (6);
+        System.out.println(a+b);   
+    }
+}
