85f23a027166aaa08d4dc24d50389e92824315c1
Ushant Singh <ushantsingh4@gmail.com>
2025-11-11 13:22:15 +0530

    first non repeating characters Ex:- aabbcd. ---> c

---
+++ lec6.java/NonRepeating_Characters.java
@@ -0,0 +1,31 @@
+// first non repeating characters Ex:- aabbcd. ---> c 
+
+public class NonRepeating_Characters {
+
+    void FirstNonRepeating(String str) {
+        // String str = "ushant singh";
+        int freq[] = new int[256];
+        for (int ch : str.toCharArray()) {
+            freq[ch]++;
+        }
+        // System.out.println("Frequency of Characters are : ");
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] > 0) {
+                // System.out.println((char) i + "-->" + freq[i]);
+            }
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (freq[str.charAt(i)] == 1) {
+                System.out.println("First non-repeating character: " + str.charAt(i));
+                return;
+            }
+        }
+        System.out.println("All characters are repeating");
+    }
+
+    public static void main(String[] args) {
+        NonRepeating_Characters obj = new NonRepeating_Characters();
+        obj.FirstNonRepeating("aabbcdd");
+    }
+}
