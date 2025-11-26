bf81d25acdd366175565ebafc3c9833d9223a57d
Ushant Singh <ushantsingh4@gmail.com>
2025-11-26 16:22:20 +0530

    Questoin 3

--- lec14/Hasmap.java
+++ lec14/Hasmap.java
@@ -2,22 +2,34 @@ import java.util.HashMap;
 
 public class Hasmap {
     public static void main(String[] args) {
-        String str="aabcad";
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char ch : str.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        int arr[] = { 2, 7, 5 };
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int target = 9;
+    for (int i = 0; i < arr.length; i++) {
+        int diff = target - arr[i];
+        if (map.containsKey(diff)) {
+            System.out.println(map.get(diff) + " " + i);
+            break;
         }
-        System.out.println(map);
+        map.put(arr[i], i);
+    }
 
-        for (char ch : str.toCharArray()) {
-            if (map.get(ch)==1) {
-                System.out.println(ch);
-                break;
-            }
-        }
+
         
         
-        
+        // String str="aabcad";
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // for (char ch : str.toCharArray()) {
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // }
+        // System.out.println(map);
+
+        // for (char ch : str.toCharArray()) {
+        //     if (map.get(ch)==1) {
+        //         System.out.println(ch);
+        //         break;
+        //     }
+        // }
         
         // HashMap<Integer, Integer> map = new HashMap<>();
         // int arr[] = { 2, 2, 3, 4, 2, 4 };
