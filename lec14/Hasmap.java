3da3bffbf263cfe827866ac5862a26620ef30b24
Ushant Singh <ushantsingh4@gmail.com>
2025-11-26 15:51:40 +0530

    Question 2

--- lec14/Hasmap.java
+++ lec14/Hasmap.java
@@ -2,18 +2,29 @@ import java.util.HashMap;
 
 public class Hasmap {
     public static void main(String[] args) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int arr[] = { 2, 2, 3, 4, 2, 4 };
-        for (int x : arr) {
-            map.put(x,map.getOrDefault(x, 0)+1);
+        String str="aabcad";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
         System.out.println(map);
 
-
-
-
-
-
+        for (char ch : str.toCharArray()) {
+            if (map.get(ch)==1) {
+                System.out.println(ch);
+                break;
+            }
+        }
+        
+        
+        
+        
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // int arr[] = { 2, 2, 3, 4, 2, 4 };
+        // for (int x : arr) {
+        //     map.put(x,map.getOrDefault(x, 0)+1);
+        // }
+        // System.out.println(map);
 
     //     map.put(1, 10);
     //     map.put(2  , 20);
