3fe566e94da06bfebe8df505ee133670419fde96
Ushant Singh <ushantsingh4@gmail.com>
2025-12-09 14:19:21 +0530

    t

--- lec18/ExceptionHandling.java
+++ lec18/ExceptionHandling.java
@@ -11,6 +11,20 @@ public class ExceptionHandling {
         } catch (Exception e) {
             System.out.println(e);
         }
+
+        try {
+            int arr[] = { 1, 2, 3, 4 };
+            System.out.println(arr[5]);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        try {
+            String str = null;
+            System.out.println(str);
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
         // int a = 5;
         // int b = 0;
         // int c = a / b;
