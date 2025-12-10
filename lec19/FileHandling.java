ae7d15cf816d4d70dc9bc0ef963178566591423b
Ushant Singh <ushantsingh4@gmail.com>
2025-12-10 15:50:38 +0530

    File Handling

--- lec19/FileHandling.java
+++ lec19/FileHandling.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 
 public class FileHandling {
@@ -14,16 +15,30 @@ public class FileHandling {
             System.out.println(e);
         }
         createData();
+        readData();
     }
     
     static void createData() {
         try {
-            FileWriter writer = new FileWriter("book1.html",true);
+            FileWriter writer = new FileWriter("book1.html", true);
             writer.write("hello hii");
             writer.write("\nI am Samarth ");
             writer.close();
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+    
+    static void readData() {
+        try {
+            FileReader reader = new FileReader("book1.html");
+            int ch;
+            while ((ch = reader.read()) != -1) {
+                System.out.print((char)ch);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
