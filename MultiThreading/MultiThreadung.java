2ab865860c7d957dc397d8603000416c7f715977
Ushant Singh <ushantsingh4@gmail.com>
2025-12-11 11:07:56 +0530

    Multi-Threading

--- MultiThreading/MultiThreadung.java
+++ MultiThreading/MultiThreadung.java
@@ -14,7 +14,14 @@
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName());
         MyThread t1 = new MyThread();
-        t1.start();
+        t1.start();// we have to use start() to start the thread
+        t1.setName("Thread 1");
+
+        try {
+            t1.join();//--> it runs the the Thread whic is first called & blocks the rest Threads
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         for (int i = 0; i < 5; i++) {
             System.out.println(i+" main");
         }
