6a4fd0528a4251048848ccb07aacf070f837bf9d
Ushant Singh <ushantsingh4@gmail.com>
2025-12-11 12:21:41 +0530

    Somgs Muti threading

--- MultiThreading/SongDemo.java
+++ MultiThreading/SongDemo.java
@@ -1,6 +1,14 @@
 class Song extends Thread {
     public void run() {
-        String str = "Yup Yup Yup haa haa Hee hee ";
+        String str = "कमर up कमर up कमर up\n" + //
+                        "कमर down कमर down\n" + //
+                        "कमर up\n" + //
+                        "कमर down कमर down\n" + //
+                        "\n" + //
+                        "गोरे गोरे मुख प कला कला केसिया\n" + //
+                        "गोरे गोरे मुखप काला काला केसिया\n" + //
+                        "लागे दोनों अखिया brown\n" + //
+                        "ये...";
         for (char ch : str.toCharArray()) {
             System.out.print(ch);
             try {
