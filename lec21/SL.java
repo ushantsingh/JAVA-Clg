0d1696f88d2f9a7d80fa3b2a38625ab2fa11d67a
Ushant Singh <ushantsingh4@gmail.com>
2026-01-19 15:56:08 +0530

    LL

--- lec21/SL.java
+++ lec21/SL.java
@@ -9,21 +9,51 @@ class Node {
 }
 
 class MyLinkedList{
-    Node head;
+    Node first;
 
     void inserBegning(int data) {
         Node newNode = new Node(data);
-        newNode.next = head;
-        head = newNode;
+        newNode.next = first;
+        first = newNode;
     }
     
     void display() {
-        Node temp = head;
+        Node temp = first;
         while (temp != null) {
             System.out.print(temp.data + "-->");
             temp = temp.next;
         }
         System.out.println("END");
+    }
+
+    void insertLast(int data) {
+        Node newNode = new Node(data);
+        if (first == null) {
+            first = newNode;
+        }
+        Node temp = first;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    void deleteStart() {
+        if (first == null) {
+            return;
+        }
+
+        // Node temp = first;
+        first = first.next;
+        // temp.next = null;
+    }
+
+    void deleteLast() {
+        Node temp = first;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
     }
 }
 public class SL {
@@ -31,6 +61,9 @@ public class SL {
         MyLinkedList list = new MyLinkedList();
         list.inserBegning(10);
         list.inserBegning(20);
+        list.insertLast(30);
+        // list.deleteStart();
+        list.deleteLast();
         list.display();
     }
 }
